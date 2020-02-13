7ba45c49fb0c5b85952b75d71f3e2ec50b22b42e
Daniel Tello Gaete <dtellogaete@gmail.com>
2020-02-13 12:56:14 -0300

    linear regression

--- Regresión Lineal/linear_regression_gd.R
+++ Regresión Lineal/linear_regression_gd.R
@@ -2,9 +2,9 @@
 
 LinearRegressionGD = function(lrate = 0.1, niter = 10000,
                               X, y, theta){
+  const = lrate*(1/length(X))
   for(i in 1:niter){
     h = X*theta[2]+theta[1]
-    const = lrate*(1/length(X))
     theta[1] = theta[1]-const*(sum(h-y))
     theta[2] = theta[2]-const*(sum(h-y))*X
   }
@@ -12,51 +12,52 @@ LinearRegressionGD = function(lrate = 0.1, niter = 10000,
 }
 
 # Importar el dataset
-dataset = read.csv('consumo_cerveja.csv', sep = ",")
-dataset = dataset[1:365, c(2,7)]
-dataset$Temperatura.Media..C. = as.numeric(dataset$Temperatura.Media..C.)/10
+dataset = read.csv('Admission_Predict_Ver1.1.csv', sep = ",")
+dataset = dataset[1:length(dataset$GRE.Score), c(2,9)]
 
 # Selección conjunto de entrenamiento y test
 library(caTools)
 set.seed(0)
-split = sample.split(dataset$Temperatura.Media..C., SplitRatio = 0.75)
+split = sample.split(dataset$GRE.Score, SplitRatio = 0.75)
 training = subset(dataset, split == TRUE)
 testing = subset(dataset, split == FALSE)
 
-# Aplicación del modelo
-linearRegression = LinearRegressionGD(lrate = 0.0005, niter = 10000,
-                                      X = training$Temperatura.Media..C.,
-                                      y = training$Consumo.de.cerveja..litros.,
-                                      c(20.0, 0.6))
+# Escalado de variables
+training = data.frame(scale(training))
+testing = data.frame(scale(testing))
 
-ypred = testing$Temperatura.Media..C.*linearRegression[2]+
+# Aplicación del modelo
+linearRegression = LinearRegressionGD(lrate = 0.1, niter = 20000,
+                                      X = as.numeric(training$GRE.Score),
+                                      y = as.numeric(training$Chance.of.Admit),
+                                      c(0.1, 1.0))
+
+ypred = testing$GRE.Score*linearRegression[2]+
   linearRegression[1]
 
 # Aplicación del modelo con la librería de R 
-regression = lm(formula =  Consumo.de.cerveja..litros. ~ Temperatura.Media..C.,
+regression = lm(formula =  Chance.of.Admit ~ GRE.Score,
                data = training)
-
-
 
 # Representación Grafíca
 library(ggplot2)
 
 ggplot()+
-  geom_point(aes(x=testing$Temperatura.Media..C.,
-                 y=testing$Consumo.de.cerveja..litros.),
+  geom_point(aes(x=testing$GRE.Score,
+                 y=testing$Chance.of.Admit),
              colour = "red")+
-  geom_line(aes(x=testing$Temperatura.Media..C.,
+  geom_line(aes(x=testing$GRE.Score,
                 y=ypred, colour = "blue"),
              alpha = 1,
             size= 0.8)+
-  geom_line(aes(x=testing$Temperatura.Media..C.,
-                y=predict(regression, newdata = testing),
-                colour = "forestgreen"), alpha = 1,
-            size= 0.8)+
+  geom_line(aes(x=testing$GRE.Score,
+                 y=predict(regression, newdata = testing),
+                 colour = "green"), alpha = 1,
+             size= 0.8)+
   scale_color_discrete(name = "Modelo", labels = c("Descenso por gradiente", 
                                                    "Regresión Lineal (lm)"))+
-  ggtitle("Consumo de Cerveza vs Temperatura Media (Conjunto de testing) ")+
-  xlab("Temperatura Media en Sao Paulo (°C)")+
-  ylab("Consumo de cerveza (L)")
+  ggtitle("Probabilidad admisión vs GRE Score (Conjunto de Test)")+
+  xlab("GRE Score")+
+  ylab("Probabilidad admisión a Postgrado")
  
 
--- Regresión Lineal/linear_regression_gd.py
+++ Regresión Lineal/linear_regression_gd.py
@@ -5,10 +5,10 @@ Created on Tue Jan 14 16:41:52 2020
 @author: Daniel
 """
 
+# Importar las librerías 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
 
 # Algoritmo de Descenso de Gradiente 
 class LinearRegressionGD(object):
@@ -19,10 +19,10 @@ class LinearRegressionGD(object):
         
     def fit(self, X, y, theta):
         self.theta = theta
-        X_value = X[:,1].reshape(-1, 1)        
+        X_value = X[:,1].reshape(-1, 1)
+        const = self.l_rate*(1/X.shape[0])        
         for i in range(0, self.n_iter):
-            h = X.dot(self.theta)
-            const = self.l_rate*(1/X.shape[0])
+            h = X.dot(self.theta)            
             self.theta[0] = self.theta[0]-const*sum(h-y)
             self.theta[1] = self.theta[1]-const*sum((h-y).transpose().dot(X_value))            
         return self.theta
@@ -31,37 +31,45 @@ class LinearRegressionGD(object):
         X_test = X[:, 1]
         predict_value = X_test*self.theta[1]+self.theta[0]
         return predict_value   
-
-# Importar el dataset
-dataset = pd.read_csv('Consumo_cerveja.csv')
-X = list(dataset.iloc[:365, 2].values)
-X = np.asarray([float(a.replace(",",".")) for a in X])
+       
+# Importar el dataset de entranamiento
+dataset = pd.read_csv('Admission_Predict_Ver1.1.csv')
+X = dataset.iloc[:len(dataset), 1].values
 X = X.reshape(-1,1)
 X = np.insert(X, 0, 1, axis = 1)
-y = dataset.iloc[:365, 6].values
+y = dataset.iloc[:len(dataset), -1].values.reshape(-1,1)
 
 # Seleccionar conjunto de training y test
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, 
-                                                    random_state = 0)   
-   
-# Aplicación del modelo
-regression = LinearRegressionGD(l_rate = 0.006, n_iter = 100000)
-coef = regression.fit(X_train, y_train, np.array([20, 0.6]))
+                                                    random_state = 0)
+
+# Escalado de las variables
+from sklearn.preprocessing import StandardScaler
+st_x = StandardScaler()
+X_train = st_x.fit_transform(X_train)
+X_test = st_x.transform(X_test)
+st_y = StandardScaler()
+y_train = st_y.fit_transform(y_train).reshape(-1)
+y_test = st_y.transform(y_test).reshape(-1)
+
+# Aplicación del modelo con grad
+regression = LinearRegressionGD(l_rate = 0.01, n_iter = 20000)
+coef = regression.fit(X_train, y_train, np.array([0.0, 1]))
 y_predict = regression.predict(X_test)
 
 # Aplicación del modelo con librería de sklearn
 from sklearn.linear_model import LinearRegression
 regression_py = LinearRegression() 
 regression_py.fit(X_train, y_train)
-y_predict_py = regression_py.predict(X_test)
+y_predict_py = regression_py.predict(X_test) 
 
 # Gráfica de regresión conjunto de test
 plt.scatter(X_test[:,1], y_test, color = "red") 
-plt.plot(X_test[:,1], y_predict, color = "blue") 
+plt.plot(X_test[:,1], y_predict_py, color = "blue")
 plt.legend(('Descenso de Gradiente',),
-           loc='lower right')
-plt.title("Consumo de cerveza vs Temperatura Sao Paulo (Conjunto de Test)")
-plt.xlabel("Temperatura °C Sao Paulo")
-plt.ylabel("Consumo cerveza en Litros")
+            loc='lower right')
+plt.title("Probabilidad admisión vs GRE Score (Conjunto de Test)")
+plt.xlabel("GRE Score")
+plt.ylabel("Probabilidad admisión a Postgrado")
 plt.show()
